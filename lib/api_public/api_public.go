1d21d439033c337ecc3f7e9b7da32118d3b3132b
mukhtar-syarief <mukhtar.syariefudin@gmail.com>
2023-06-12 17:59:20 +0700

    fixing public request

--- lib/api_public/api_public.go
+++ lib/api_public/api_public.go
@@ -64,8 +64,9 @@ func (api *TokopediaApiPublic) SendRequest(req *http.Request, hasil any) error {
 
 func defaultHeader() map[string]string {
 	headers := map[string]string{
-		"content-type": "application/json",
-		"origin":       "https://www.tokopedia.com",
+		"Content-Type": "application/json",
+		"Origin":       "https://www.tokopedia.com",
+		"Accept":       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 		"User-Agent":   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
 	}
 	return headers
