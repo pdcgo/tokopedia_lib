2db8086c71d8a13866257069d5032d503802234f
vaziria <manorder123@gmail.com>
2023-06-17 14:31:27 +0700

    tampilan toped

---
+++ app/web/assets/frontend/assets/js/nox_krekpQe7r.js
@@ -0,0 +1,1 @@
+import{r as t}from"./nox_ophygXhXg.js";const s=t.createContext({}),d=a=>{let{children:c,status:o,override:n}=a;const r=t.useContext(s),m=t.useMemo(()=>{const e=Object.assign({},r);return n&&delete e.isFormItemInput,o&&(delete e.status,delete e.hasFeedback,delete e.feedbackIcon),e},[o,n,r]);return t.createElement(s.Provider,{value:m},c)};export{s as F,d as N};
