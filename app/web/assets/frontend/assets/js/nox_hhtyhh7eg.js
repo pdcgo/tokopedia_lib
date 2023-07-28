cba8a77f8e5d6443e9a855af22605b8c8eb3c431
vaziria <manorder123@gmail.com>
2023-07-28 17:54:41 +0700

    change ignore asset

--- app/web/assets/frontend/assets/js/nox_hhtyhh7eg.js
+++
@@ -1,1 +0,0 @@
-import{q as r,F as t}from"./nox_gAA7QekX7.js";import{T as l}from"./nox_hpe7tttkA.js";function a(e){return r.jsxs(t,{style:{rowGap:"5px",...e.style},children:[r.jsx(l.Text,{children:e.label}),e.children]})}export{a as L};
